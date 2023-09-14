fb070599cba9d3699cdb26d5ae37d37b719b6f4c
freankine212 <lilmichaeljr@gmail.com>
2023-09-13 22:47:13 -0600

    yes

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const date = new Date(document.lastModified);
+document.getElementById("date").innerHTML = date; 
+
+document.getElementById("theCurrentYear").innerHTML = new
+   Date().getFullYear();
